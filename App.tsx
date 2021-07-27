0df3245bf6a73e9e4e4cb72fd86f2788aab4bb92
Hélvio Filho <helviosvf@gmail.com>
2021-07-26 23:10:31 -0300

    Added fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { StyleSheet, Text, View } from 'react-native';
+import { CustomText, Logo } from './src/components';
+
+import { defaultTheme } from './src/global/styles/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={defaultTheme}>
+      <View style={styles.container}>
+        <Logo />
+        <CustomText>Apenas um texto</CustomText>
+        {/* <Text>Apenas um texto</Text> */}
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#161616',
     alignItems: 'center',
     justifyContent: 'center',
   },
