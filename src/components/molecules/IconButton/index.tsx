fb0265c28eec86fec4997c9aa01297c1471bf917
Hélvio Filho <helviosvf@gmail.com>
2021-07-29 02:34:18 -0300

    Added icon button

---
+++ src/components/molecules/IconButton/index.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { CustomText } from '../../atoms';
+import { defaultTheme } from '../../../global/styles/theme';
+
+import { ButtonContainer } from './styles';
+
+interface IconProps {
+  name: React.ComponentProps<typeof Ionicons>['name'];
+  onPress: () => void;
+  label: ReactNode;
+}
+
+export function IconButton({ name, label, onPress }: IconProps) {
+  return (
+    <ButtonContainer onPress={onPress}>
+      <Ionicons
+        name={name}
+        size={defaultTheme.metrics.px(24)}
+        color={defaultTheme.colors.white}
+      />
+      <CustomText fontFamily="semiBold" size={10} mt={6}>
+        {label}
+      </CustomText>
+    </ButtonContainer>
+  );
+}
